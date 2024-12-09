ac39bbb921b3d8749f5a254a864d4b2e8538e9a4
bobokrut <eg.evlampiev@gmail.com>
2024-12-09 12:31:44 +0100

    chore: updated logo

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ def create_layout() -> list[my_dash_component.Container | dcc.Graph]:
             my_dash_component.Navbar(
                 id="sag_navbar",
                 dashboard_name=dashboard.service.name,
-                dashboard_picture="https://www.fh-krems.ac.at/fileadmin/imc/images/logos/imc-logo-web-preview.png",
+                dashboard_picture="https://www.imc.ac.at/_assets/b47bb453db4183e9c517c3552cad317b/Images/logo-full.svg",
                 dashboard_version=str(dashboard.service.version),
             ),
             my_dash_component.Grid(
